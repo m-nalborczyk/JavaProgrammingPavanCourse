454f1c3faccd9744a5afa143edd205eea2c576a1
nalboro <stev1989@gmail.com>
2024-03-03 21:16:01 +0100

    Second commit

---
+++ src/day1/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class FirstJavaProgram {
+
+    public static void main (String args[]){
+        System.out.println("welcome to java");
+        System.out.println(12+20);
+
+    }
+
+}
---
+++ src/day1/SecondJavaProgram.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class SecondJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("welcome in the 2nd program");
+    }
+
+}
