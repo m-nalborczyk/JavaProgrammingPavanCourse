154ddb3c135587e2c341cbb465d1dfa36d38c238
nalboro <stev1989@gmail.com>
2024-03-03 21:41:20 +0100

    ammendment

--- src/day1/FirstJavaProgram.java
+++ src/day1/FirstJavaProgram.java
@@ -2,7 +2,7 @@ package day1;
 
 public class FirstJavaProgram {
 
-    public static void main (String args[]){
+    public static void main (String[] args){
         System.out.println("welcome to java");
         System.out.println(12+20);
 
