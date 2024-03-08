a0ca8fe5caf52e71616be064cd0fa31b502e9c1f
nalboro <stev1989@gmail.com>
2024-03-08 23:37:29 +0100

    commit AssignmentConditionalsAndLoops

---
+++ src/day5/AssignmentConditionalsAndLoops.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class AssignmentConditionalsAndLoops {
+    public static void main(String[] args) {
+
+
+//        1) Reverse a number (  %   / =   operators   )//
+//        input 1234
+//        output 4321
+//
+//        2) Palindrome number
+//        input 121
+//        after reversing 121
+//
+//        3) Count Number of Digits in a Number//
+//        Input : 423424
+//        output : 6
+//
+//        4) count Number of Even and Odd digits in a Number
+//        Input: 23456
+//        Output : 3 even  2 odd
+////
+//        5) Find sum of digits in a number
+//        input : 1234
+//        sum= 10
+//
+
+
+
+    }
+}
