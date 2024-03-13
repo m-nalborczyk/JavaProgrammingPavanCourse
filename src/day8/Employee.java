33213ef6b6b7af4d87c311b0c5e41e04700cedc4
nalboro <stev1989@gmail.com>
2024-03-13 13:45:01 +0100

    commit Employee blueprint

---
+++ src/day8/Employee.java
@@ -0,0 +1,20 @@
+package day8;
+
+public class Employee {
+
+    int employeeId;
+    String employeeName;
+    String job;
+    int employeeSalary;
+
+    void display(){
+        System.out.println(employeeId);
+        System.out.println(employeeName);
+        System.out.println(job);
+        System.out.println(employeeSalary);
+    }
+
+
+
+
+}
