267d4295245dd3ccb6d512c7ea2c0f754ff8aa2f
nalboro <stev1989@gmail.com>
2024-03-24 10:34:43 +0100

    commit FinallyBlock

---
+++ src/day14/FinallyBlock.java
@@ -0,0 +1,33 @@
+package day14;
+
+import java.util.Scanner;
+
+public class FinallyBlock {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program is  started..");
+		System.out.println("Program is  in progress..");
+		
+		String s=null;
+		
+		try
+		{
+			System.out.println(s.length());
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Exception occured...handled");
+			
+		}
+		finally
+		{
+			System.out.println("Entered in to finally block.......");
+		}
+		
+		System.out.println("Program is  completed..");
+		System.out.println("Program is  exited..");
+
+	}
+
+}
