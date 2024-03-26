cffd71e7e8398b3c466684a6fa23da7dcb33e45c
nalboro <stev1989@gmail.com>
2024-03-26 15:05:27 +0100

    commit StaticExampleMain changes

--- src/day10/StaticExampleMain.java
+++ src/day10/StaticExampleMain.java
@@ -9,17 +9,18 @@ public class StaticExampleMain {
 
     public static void main(String[] args) {
 
-        System.out.println(a); //directly accessing static variable without object creation
-        m1(); //directly accessing static method without object creation
+        System.out.println(a); //directly accessing static variable without object creation BUT import static day10.StaticExample.a; above is required
+        m1(); //directly accessing static method without object creation BUT import static day10.StaticExample.m1; above is required
         StaticExample.m3(); //directly accessing static method but without using "import static PATH.VARIABLE_NAME"
+        //by directly we mean without creating object of the class StaticExample
 
         StaticExample se = new StaticExample(); //non-static method needs object creation
-        se.m2();
+        se.m2(); //no import here required as object is created
 
-        System.out.println(); // the same principle as for calling predefined  methods within different classes to static variable as below > StaticExample.word.length()
+        System.out.println(); // the same principle as for calling predefined methods within different classes to static variable as below > StaticExample.word.length()
         System.out.println(StaticExample.word.length());
         //"System" is class
-        //"out" is static variable of PrintStream type
+        //"out" is static variable of PrintStream type in System class
         //"println() is one of predefined methods for PrintStream class
 
 
